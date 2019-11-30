4d5c50d22037e92c2a21b7da7908abb102813831
mmorganufl <mmorganufl@gmail.com>
2019-11-30 14:29:43 -0500

    i wrote a program that tells me the x,y, and vy position.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print ("Hello world")
+import math
+
+def plot(x, y):
+    print("x = " + str(x))
+    print("y = " + str(y))
+
+#startingspeed=int(input("what is the starting speed"))
+#startingangle=int(input("what is the starting angle"))
+startingspeed=100
+startingangle=45
+x=0
+y=0
+
+vx = startingspeed*math.cos(math.radians(startingangle))
+vy = startingspeed*math.sin(math.radians(startingangle))
+
+t=.0001
+timeinair=0
+
+while(y>=0):
+    x=x+t*vx
+    y=y+t*vy
+    vy=vy-t*9.8
+
+    # this calculates the new x value
+    timeinair=timeinair+t
+    plot(x,y)
+print(str (timeinair))
